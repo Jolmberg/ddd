6dc10715b4bf82322303405bc2d84d80506cbd30
Johannes Holmberg <johannes@update.uu.se>
2018-11-03 21:46:52 +0100

    burk

--- ddd.c
+++ ddd.c
@@ -2,9 +2,11 @@
 #include <stdint.h>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_surface.h>
 
 #include "motherboard.h"
 #include "8088.h"
+#include "text.h"
 
 int main(int argc, char *argv[])
 {
@@ -18,17 +20,42 @@ int main(int argc, char *argv[])
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
         return 3;
     }
-
-    if (SDL_CreateWindowAndRenderer(320, 240, SDL_WINDOW_SHOWN, &window, &renderer)) {
+    window = SDL_CreateWindow("Flork", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 320, 240, 0);
+    if (!window) {
+    //if (SDL_CreateWindowAndRenderer(320, 240, SDL_WINDOW_SHOWN, &window, &renderer)) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
         return 3;
     }
 
-    surface = IMG_Load("asciialpha.png");
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    text_init();
+    //surface = IMG_Load("asciialpha.png");
+    Uint32 rmask, gmask, bmask, amask;
+
+    /* SDL interprets each pixel as a 32-bit number, so our masks must depend
+       on the endianness (byte order) of the machine */
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    rmask = 0xff000000;
+    gmask = 0x00ff0000;
+    bmask = 0x0000ff00;
+    amask = 0x000000ff;
+#else
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = 0xff000000;
+#endif
+
+    surface = SDL_CreateRGBSurface(0, 320, 240, 32, rmask, gmask, bmask, amask);
     if (!surface) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", IMG_GetError());
         return 3;
     }
+    printf("humbug\n");
+    sdlprintf(surface, 20, 40, "broffe");
+    printf("asd\n");
+
     texture = SDL_CreateTextureFromSurface(renderer, surface);
     if (!texture) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
@@ -37,14 +64,15 @@ int main(int argc, char *argv[])
     SDL_FreeSurface(surface);
     SDL_SetTextureColorMod(texture, 255, 0, 0);
     SDL_Rect rect = { 10, 10, 288, 128 };
+    SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
     while (1) {
         SDL_PollEvent(&event);
         if (event.type == SDL_QUIT) {
             break;
         }
-        SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0x00);
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, &rect);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
 	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0x00);
 	SDL_RenderDrawLine(renderer, 10,10,100,100);
         SDL_RenderPresent(renderer);
--- text.c
+++ text.c
@@ -2,21 +2,38 @@
 #include <stdarg.h>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_surface.h>
 
+SDL_Surface *asciialpha;
 
-int vsdlprintf(SDL_Surface *surface, char *fmt, va_list argp)
+void text_init(void)
+{
+    asciialpha = IMG_Load("asciialpha.png");
+}
+
+int vsdlprintf(SDL_Surface *surface, int x, int y, char *fmt, va_list argp)
 {
     static char buffer[1024];
     vsprintf(buffer, fmt, argp);
-    // sdl-flork
-   
+    printf("%s\n", buffer);
+    char *b = buffer;
+    int i = 0;
+    while (*b) {
+	int sx = *b & 31;
+	int sy = *b >> 5;
+	SDL_Rect srcrect = { sx*9, sy*16, 9, 16 };
+	SDL_Rect dstrect = { x + i*9, y, 9, 32 };
+	SDL_BlitSurface(asciialpha, &srcrect, surface, &dstrect);
+	b++;
+	i++;
+    }
 }
 
-int sdlprintf(SDL_Surface *surface, char *fmt, ...)
+int sdlprintf(SDL_Surface *surface, int x, int y, char *fmt, ...)
 {
     va_list argp;
     va_start(argp, fmt);
-    int r = vsdlprintf(surface, fmt, argp);
+    int r = vsdlprintf(surface, x, y, fmt, argp);
     va_end(argp);
 }
    
