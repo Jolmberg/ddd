2b7681529edf5fa0efd6239120993f0b92fd996b
Johannes Holmberg <johannes@update.uu.se>
2018-11-03 13:41:36 +0100

    flork

--- Makefile
+++ Makefile
@@ -1,13 +1,18 @@
-CC=gcc
-CFLAGS = -O2
+CC = gcc
+CFLAGS = -O2 $(shell pkg-config --cflags sdl2 sdl2_image)
 OBJECTS = ddd.o 8088.o motherboard.o
+LDFLAGS = $(shell pkg-config --libs sdl2 sdl2_image)
+
 
 ddd : $(OBJECTS)
-	$(CC) $(CFLAGS) $(OBJECTS) -o ddd
+	$(CC) $(LDFLAGS) $(SDLFLAGS) $(OBJECTS) -o ddd
 
-%.o : %.c %.h
+%.o : %.c
 	$(CC) $(CFLAGS) -c $<
 
-clean:
-	rm -rf *.o wmix *~
+8088.o : 8088.h
 
+motherboard.o : motherboard.h
+
+clean:
+	rm -rf *.o ddd *~
--- ddd.c
+++ ddd.c
@@ -1,11 +1,61 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <SDL.h>
+#include <SDL_image.h>
 
 #include "motherboard.h"
 #include "8088.h"
 
 int main(int argc, char *argv[])
 {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+    SDL_Event event;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
+    }
+
+    if (SDL_CreateWindowAndRenderer(320, 240, SDL_WINDOW_SHOWN, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+        return 3;
+    }
+
+    surface = IMG_Load("ascii.png");
+    if (!surface) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", IMG_GetError());
+        return 3;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (!texture) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
+        return 3;
+    }
+    SDL_FreeSurface(surface);
+    SDL_SetTextureColorMod(texture, 255, 0, 0);
+    SDL_Rect rect = { 10, 10, 288, 128 };
+    while (1) {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT) {
+            break;
+        }
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0x00);
+	SDL_RenderDrawLine(renderer, 10,10,100,100);
+        SDL_RenderPresent(renderer);
+    }
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
     struct motherboard *mb = mb_create();
     mb_load_bios_rom(mb, "BIOS_5150_24APR81_U33.BIN");
     mb_powerup(mb);
