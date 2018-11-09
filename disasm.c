0d00120c408f61b9aac09d1666bdd651987b3e1b
Johannes Holmberg <johannes@update.uu.se>
2018-11-09 13:53:37 +0100

    flork

--- disasm.c
+++ disasm.c
@@ -4,6 +4,12 @@
 
 #define IS_SEGMENT_OVERRIDE(x) (((x) & 0xE7) == 0x66)
 
+/* Variable format: $(operand position)(operand type)
+   Types: i8 - byte
+          i16 - word
+          oN - opcode name is in extended list N at index given by the next byte
+          n - modxxxr/m byte
+          m - modregr/m byte */
 char instr_format[256][20] =
 {
     "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
@@ -13,20 +19,20 @@ char instr_format[256][20] =
     "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
     "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
     "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "JAE $1i8", "", "JNE $1i8", "", "", "", "JNS $1i8", "", "JNP $1i8", "", "", "", "",
+    "", "", "", "jae $1i8", "", "jne $1i8", "", "", "", "jns $1i8", "", "jnp $1i8", "", "", "", "",
     "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "SAHF", "LAHF",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "sahf", "lahf",
     "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "MOV al, $1i8", "MOV cl, $1i8", "MOV dl, $1i8", "MOV bl, $1i8", "MOV ah, $1i8", "MOV ch, $1i8", "MOV dh, $1i8", "MOV bh, $1i8", "MOV ax, $1i16", "MOV cx, $1i16", "MOV dx, $1i16", "MOV bx, $1i16", "MOV sp, $1i16", "MOV bp, $1i16", "MOV si, $1i16", "MOV di, $1i16",
+    "mov al, $1i8", "mov cl, $1i8", "mov dl, $1i8", "mov bl, $1i8", "mov ah, $1i8", "mov ch, $1i8", "mov dh, $1i8", "mov bh, $1i8", "mov ax, $1i16", "mov cx, $1i16", "mov dx, $1i16", "mov bx, $1i16", "mov sp, $1i16", "mov bp, $1i16", "mov si, $1i16", "mov di, $1i16",
     "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "$1o0 $1mr, cl", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "JMP $3i16:$1i16", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "CLI", "", "", "", "", ""
+    "", "", "$1o0"/* $1n, cl"*/, "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "jmp $3i16:$1i16", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "cli", "", "", "", "", ""
 };
 
-char extra[][8][5] =
+const char extended[1][8][5] =
 { // shifts/rotate
-    { "ROL", "ROR", "RCL", "RCR", "SHL", "SHR", "", "SAR" }
+    { "rol", "ror", "rcl", "rcr", "shl", "shr", "", "sar" }
 };
 
 
@@ -34,7 +40,6 @@ char extra[][8][5] =
 int sprint_instruction_at_address(char *buffer, struct motherboard *mb, uint32_t address) {
     int segment_override = -1;
     uint8_t b = mb_memory_peek(mb, address);
-    
     if (IS_SEGMENT_OVERRIDE(b)) {
 	segment_override = b;
 	b = mb_memory_peek(mb, ++address);
@@ -50,26 +55,32 @@ int sprint_instruction_at_address(char *buffer, struct motherboard *mb, uint32_t
             format++;
         } else {
             int operand = format[1] - '0';
-	    operand_distance = operand;
+            operand_distance = operand;
             format += 2;
             if (!strncmp(format, "i8", 2)) {
                 int p = sprintf(buffer, "0x%x", mb_memory_peek(mb, address + operand));
                 buffer += p;
                 format += 2;
             } else if (!strncmp(format, "i16", 3)) {
-		operand_distance++;
+                operand_distance++;
                 int word = mb_memory_peek(mb, address + operand)
-		    | (mb_memory_peek(mb, address +operand + 1) << 8);
+                    | (mb_memory_peek(mb, address + operand + 1) << 8);
                 int p = sprintf(buffer, "0x%x", word);
-                operand += 2;
                 buffer += p;
                 format += 3;
+            } else if (!strncmp(format, "o", 1)) {
+                int list = format[1] - '0';
+                int number = mb_memory_peek(mb, address + operand);
+                number = (number >> 3) & 7;
+                int p = sprintf(buffer, extended[list][number]);
+                buffer += p;
+                format += 2;
             } else {
                 format++; // Just get us out of here!
             }
-	    if (operand_distance > max_distance) {
-		max_distance = operand_distance;
-	    }
+            if (operand_distance > max_distance) {
+                max_distance = operand_distance;
+            }
         }
     }
     buffer[0] = format[0];
