b02e17815dc32843e88a8df06819fb934da7058f
Johannes Holmberg <johannes@update.uu.se>
2018-11-12 13:34:53 +0100

    blork

--- 8088.c
+++ 8088.c
@@ -27,64 +27,6 @@ const uint8_t instruction_length[256] =
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1,
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
-
-char instruction_format[256][20] =
-{
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "JAE $1i8", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "SAHF", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "MOV al, $1i8", "MOV cl, $1i8", "MOV dl, $1i8", "MOV bl, $1i8", "MOV ah, $1i8", "MOV ch, $1i8", "MOV dh, $1i8", "MOV bh, $1i8", "MOV ax, $1i16", "MOV cx, $1i16", "MOV dx, $1i16", "MOV bx, $1i16", "MOV sp, $1i16", "MOV bp, $1i16", "MOV si, $1i16", "MOV di, $1i16",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "JMP $3i16:$1i16", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "CLI", "", "", "", "", ""
-};
-
-
-void sprint_instruction(char *buffer, struct iapx88 *cpu) {
-    char *format = instruction_format[cpu->cur_inst[0]];
-    int i;
-    while (format[0]) {
-        if (format[0] != '$') {
-            buffer[0] = format[0];
-            buffer++;
-            format++;
-        } else {
-            int operand = format[1] - '0';
-            format += 2;
-            if (!strncmp(format, "i8", 2)) {
-                int p = sprintf(buffer, "0x%x", cpu->cur_inst[operand]);
-                buffer += p;
-                format += 2;
-            } else if (!strncmp(format, "i16", 3)) {
-                int word = cpu->cur_inst[operand] | (cpu->cur_inst[operand + 1] << 8);
-                int p = sprintf(buffer, "0x%x", word);
-                operand += 2;
-                buffer += p;
-                format += 3;
-            } else {
-                format++; // Just get us out of here!
-            }
-        }
-    }
-    buffer[0] = format[0];
-}
-
-void print_instruction(struct iapx88 *cpu)
-{
-    char buffer[30];
-    sprint_instruction(buffer, cpu);
-    printf("%s\n", buffer);
-}
-
 struct iapx88 *iapx88_create(void)
 {
     struct iapx88 *cpu = (struct iapx88 *)malloc(sizeof(struct iapx88));
@@ -213,7 +155,6 @@ int iapx88_step(struct iapx88 *cpu)
 	    cpu->state = CPU_DECODE;
 	    break;
 	case CPU_DECODE:
-            print_instruction(cpu);
 	    if (cpu->cur_inst_len == 0) {
 		return -1;
 	    }
